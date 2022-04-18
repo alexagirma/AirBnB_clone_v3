c274b13ee1fd8413941c49dde7a315607504af3b
Susiniosgit <magnawlege@gmail.com>
2022-04-18 11:05:35 +0000

    semicolon added

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,37 +1,29 @@
 #!/usr/bin/python3
 """
-A python script that starts a Flask web application
+ Start Flask Application
 """
-from flask import Flask, Blueprint, abort, jsonify
 from models import storage
+from flask import Flask, Blueprint, make_response, jsonify
+from flask_cors import CORS
 from api.v1.views import app_views
 from os import getenv
-from flask_cors import CORS
-
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-app.register_blueprint(app_views)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    """
-    Function that shows a 404 error
-    """
-    return (jsonify(error="Not found"), 404)
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def teardown(exception=None):
-    """
-     Function closes the current session
-    """
+def finish(NaN):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 if __name__ == '__main__':
-    h = getenv('HBNB_API_HOST')
-    p = getenv('HBNB_API_PORT')
-    app.run(host=h, port=p, threaded=True)
+    host = getenv("HBNB_API_HOST", '0.0.0.0')
+    port = getenv("HBNB_API_PORT", '5000')
+    app.run(host=host, port=port, threaded=True, debug=True)
